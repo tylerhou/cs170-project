9e662327d1662f7491489ec37d7d149c30b28431
tylerhou@google.com <tylerhou@google.com>
2021-12-03 15:44:27 -0800

    Maintain a list of cases to solve.
    These cases were the top 400 cases (~rank 28) cases that we are
    underperforming in on the leaderboard.

--- query.js
+++ query.js
@@ -1,5 +1,20 @@
 Array.from(document.getElementById("table").children[0].children)
   .splice(1)
-  .filter((tr) => tr.children[1].innerHTML != "1")
-  .map((tr) => tr.children[0].innerText);
-
+  .map((tr) => ({
+    case: tr.children[0].innerText,
+    rank: Number(tr.children[1].innerHTML),
+  }))
+  .sort((f, s) => s.rank - f.rank)
+  .splice(0, 400)
+  .map((s) => {
+    if (s.case.startsWith("small")) {
+      return "small/" + s.case;
+    }
+    if (s.case.startsWith("medium")) {
+      return "medium/" + s.case;
+    }
+    if (s.case.startsWith("large")) {
+      return "large/" + s.case;
+    }
+  })
+  .join(";");
--- solver.sh
+++ solver.sh
@@ -4,6 +4,5 @@ mkdir -p outputs/small
 mkdir -p outputs/medium
 mkdir -p outputs/large
 
-cat unsolved |
-    # head -n 1 |
+cat tosolve |
     parallel -j 100% --progress "bash -c "$*" < inputs/{}.in > outputs/{}.out"
