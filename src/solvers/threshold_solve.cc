8f6410a12e3447c0175924413278494563a85c75
Tyler Hou <tylerhou@google.com>
2021-12-04 17:09:16 -0800

    Add new greedy solver and fix greedy tiebreak.

--- src/solvers/threshold_solve.cc
+++ src/solvers/threshold_solve.cc
@@ -7,6 +7,7 @@
 #include <random>
 #include <vector>
 
+#include "absl/container/flat_hash_set.h"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
 
@@ -15,8 +16,41 @@
 #include "src/solvers/sequence.h"
 #include "src/solvers/threshold.h"
 
-constexpr auto kIterations = 1000;
+constexpr auto kIterations = 10;
 constexpr auto kSampleSequenceSize = 1000 * 1000;
+
+Schedule greedy_solve(const Problem &problem) {
+  std::vector<int> selections;
+  absl::flat_hash_set<int> selected;
+  int time = 0;
+  const auto &tasks = problem.tasks();
+  while (selections.size() < tasks.size()) {
+    int best_task = -1;
+    int best_profit = -1;
+    for (int task = 0; task < tasks.size(); ++task) {
+      if (selected.find(task) != selected.end())
+        continue;
+      const int end_time = time + tasks[task].duration;
+      const double profit = problem.profit_for_task(task, end_time);
+      if (profit > best_profit ||
+          (profit == best_profit &&
+           tasks[task].deadline < tasks[best_task].deadline)) {
+        best_task = task;
+        best_profit = profit;
+      }
+    }
+    CHECK(best_task != -1) << "No best task found.";
+    selections.push_back(best_task);
+    selected.insert(best_task);
+    time += tasks[best_task].duration;
+  }
+  Schedule schedule{problem};
+  schedule.set_selections(selections);
+  for (auto s : selections) {
+    LOG(INFO) << s;
+  }
+  return schedule;
+}
 
 int main(int argc, char *argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -86,7 +120,8 @@ int main(int argc, char *argv[]) {
     auto profit = result.selected_profit();
     LOG(INFO) << "Run " << label << " found schedule with profit " << profit;
     if (profit > max_profit) {
-      LOG(INFO) << "Profit was better than previous best profit " << max_profit;
+      LOG(INFO) << "Profit " << profit
+                << " was better than previous best profit " << max_profit;
       max_profit = profit;
       max_schedule = result;
     }
@@ -99,27 +134,46 @@ int main(int argc, char *argv[]) {
 
   Schedule greedy_profit{problem};
   std::vector<int> selections = greedy_profit.selections();
+  auto tasks = greedy_profit.problem().tasks();
   std::sort(selections.begin(), selections.end(), [&](int f, int s) {
-    auto tasks = greedy_profit.problem().tasks();
-    return tasks[f].profit > tasks[s].profit;
+    auto tf = tasks[f], ts = tasks[s];
+    LOG(INFO) << f << " " << tf.profit << " " << tf.deadline << " " << s << " "
+              << ts.profit << " " << ts.deadline;
+    LOG(INFO) << (tf.profit > ts.profit && tf.deadline < ts.deadline);
+    if (tf.profit == ts.profit) {
+      return tf.deadline < ts.deadline;
+    }
+    return tf.profit > ts.profit;
   });
   greedy_profit.set_selections(selections);
   update(greedy_profit, "greedy_profit");
   update(quench_close(quench_random(greedy_profit)), "quench greedy_profit");
-  update(anneal(greedy_profit), "anneal greedy_profit");
+  update(quench_close(quench_random(anneal(greedy_profit))),
+         "anneal greedy_profit");
 
   Schedule greedy_profit_ratio{problem};
   selections = greedy_profit.selections();
   std::sort(selections.begin(), selections.end(), [&](int f, int s) {
     auto tasks = greedy_profit.problem().tasks();
-    return (tasks[f].profit / tasks[f].duration) >
-           (tasks[s].profit / tasks[s].duration);
+    auto tf = tasks[f], ts = tasks[s];
+    double tf_ratio = tf.profit / tf.duration,
+           ts_ratio = ts.profit / ts.duration;
+    if (tf_ratio == ts_ratio) {
+      return tf.deadline < ts.deadline;
+    }
+    return tf_ratio > ts_ratio;
   });
   greedy_profit.set_selections(selections);
   update(greedy_profit_ratio, "greedy_profit_ratio");
   update(quench_close(quench_random(greedy_profit_ratio)),
          "quench greedy_profit_ratio");
-  update(anneal(greedy_profit_ratio), "anneal greedy_profit_ratio");
+  update(quench_close(quench_random(anneal(greedy_profit_ratio))),
+         "anneal greedy_profit_ratio");
+
+  Schedule greedy = greedy_solve(problem);
+  update(greedy, "greedy");
+  update(quench_close(quench_random(greedy)), "quench greedy");
+  update(quench_close(quench_random(anneal(greedy))), "anneal greedy");
 
   for (int i = 0; i < kIterations; ++i) {
     Schedule initial{problem};
